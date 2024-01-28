12b8b6c8f05c4aa8705c39bacf975c8d8241bb3e
Tamzid Hossen <tamzidhossen366@gmail.com>
2024-01-28 16:03:36 +0600

    Quiz Project Completion

---
+++ Syntax8.js
@@ -0,0 +1,24 @@
+const correctAnswer = ["D","C","D","C","B"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+const questions = document.querySelectorAll(".question");
+
+console.log(questions[0]);
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    let score = 0;
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
+    // console.log(userAnswers);
+    userAnswers.forEach((answer,index) => {
+        if(answer == correctAnswer[index]){
+            score += 1;
+            questions[index].classList.add("correct");
+        } else {
+            questions[index].classList.add("wrong");
+        }
+    });
+    scrollTo(0,0);
+    result.classList.remove("hide");
+    result.querySelector("p").textContent = `Your score ${score}/5`;
+});
